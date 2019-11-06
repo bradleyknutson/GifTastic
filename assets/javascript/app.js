6976665741f9acaecc4b10c8ae9e77fdf1ed3aad
Bradley Knutson <bradley@bknutson.com>
2019-11-06 16:15:53 -0700

    Added offset ability and changed gif to mp4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,13 +6,15 @@ var gifTastic = {
         this.topics.forEach(topic => {
             var topicButton = $('<button>');
             topicButton.data("topic", topic);
+            topicButton.data('offset', 0);
             topicButton.addClass('topics');
             topicButton.text(topic);
             $('#buttonSection').append(topicButton);
         });
     },
     displayImages: function(e){
-        gifTastic.buildQueryUrl($(this).data('topic'));
+        gifTastic.buildQueryUrl($(this).data('topic'), $(this).data('offset'));
+        $(this).data('offset', $(this).data('offset') + 10);
         $.ajax({
             method: "GET",
             url: gifTastic.queryUrl
@@ -21,48 +23,35 @@ var gifTastic = {
             var newGifSection = $('<div>');
             newGifSection.addClass('row')
             response.data.forEach(giphy => {
-                var newGifDiv = $('<div>');
-                newGifDiv.addClass('col-lg-4');
-                var newGif = $('<img>');
-                newGif.addClass('gif');
-                newGif.attr('src', giphy.images.original_still.url);
-                newGif.data('animated', 'off');
+                var newCardDiv = $('<div class="col-lg-12 card">');
+                var newGif = $('<video controls loop class=gif>');
+                newGif.attr('src', giphy.images.original_mp4.mp4);
+                newGif.attr('type', 'video/mp4');
+                // newGif.data('animated', 'off');
+                newCardDiv.append($('<div class=card-header>').text(giphy.title + " || Rating: " + giphy.rating));
+                var newGifDiv = $("<div class=card-body>");
                 newGifDiv.append(newGif);
-                newGifDiv.append($('<p>').text(giphy.title));
-                newGifDiv.append($('<p>').text("Rating: " + giphy.rating));
-                newGifSection.append(newGifDiv);
+                newCardDiv.append(newGifDiv);
+                newGifSection.append(newCardDiv);
             });;
             $('#gifSection').prepend(newGifSection);
         });
     },
-    buildQueryUrl: function(searchTerm){
+    buildQueryUrl: function(searchTerm, offset){
         gifTastic.queryUrl = '';
         var apiKey = 'TJxF5Z9s3iaJVlJ1fV2pSiby7iqKQw3L';
         var gifUrl = 'http://api.giphy.com/v1/gifs/search';
         searchTerm = searchTerm.replace(' ', '+');
-        gifTastic.queryUrl += gifUrl + "?q=" + searchTerm + "&limit=10&api_key=" + apiKey;
+        gifTastic.queryUrl += gifUrl + "?q=" + searchTerm + "&offset=" + offset + "&limit=10&api_key=" + apiKey;
     },
     addNewButton: function(){
         event.preventDefault();
-        if($('#newTopic').val() && !gifTastic.topics.includes($('#newTopic').val())){
-            gifTastic.topics.push($('#newTopic').val());
+        if($('#newTopic').val().trim() && !gifTastic.topics.includes($('#newTopic').val().trim())){
+            gifTastic.topics.push($('#newTopic').val().trim());
             $('#newTopic').val('');
         };
         gifTastic.addButtons();
     },
-    animateGif: function(e){
-        if($(this).data('animated') === 'off'){
-            var newSource = $(this).attr('src');
-            newSource = newSource.replace(/_s.gif/g, ".gif");
-            $(this).attr('src', newSource);
-            $(this).data('animated', 'on');
-        }else{
-            var newSource = $(this).attr('src');
-            newSource = newSource.replace(/.gif/g, "_s.gif");
-            $(this).attr('src', newSource);
-            $(this).data('animated', 'off');
-        }
-    }
 }
 
 
@@ -72,6 +61,10 @@ var gifTastic = {
 $(document).ready(function () {
     gifTastic.addButtons();
     $('#addButton').on('click', gifTastic.addNewButton);
+    $('#newTopic').keypress(function(e){
+        if(e.key === "Enter"){
+            gifTastic.addNewButton();
+        }
+    });
     $(document.body).on('click', '.topics', gifTastic.displayImages);
-    $(document.body).on('click', '.gif', gifTastic.animateGif);
 });
